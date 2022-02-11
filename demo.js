85170810c4f7c706ddec5b9dd63d2e926be102fe
AbidRaza <abidraza10026@gmail.com>
2022-02-11 23:06:56 +0530

    innit

---
+++ demo.js
@@ -0,0 +1,4 @@
+var course="data structure";
+var cardno="134556554";
+var modeofpayment=prompt("mode of payment:");
+alert("course"+course+"has bought by card no"+cardno+"from"+modeofpayment);
---
+++ test.js
@@ -0,0 +1,9 @@
+let veg=prompt("enter vegitable");
+let fru=prompt("enter fruite");
+function maketarkari(veg,fru)
+{
+    let a="vegitable is"+veg+"and fruite is"+fru; 
+    return a;
+}
+let n=maketarkari(veg,fru);
+document.write(n);
